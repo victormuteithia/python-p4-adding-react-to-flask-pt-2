1df4ebc63a099087b350884ed89d9cd17ea9ec0c
victormuteithia <muteithiavictor@gmail.com>
2024-04-18 17:06:16 +0300

    Completed assignment

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import {useEffect} from "react"
+
+function App() {
+  useEffect(() => {
+    fetch("/movies")
+    .then((r) => r.json())
+    .then((movies) => console.log(movies))
+  }, [])
+
+  return (
+    <div>
+      <h1>Check the console for a list of movies</h1>
+    </div>
+  );
+}
+
+export default App;
